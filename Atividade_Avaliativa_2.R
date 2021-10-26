0ac019edb96de8268747d6239bb0c04a87a8c3cf
Higor Gomes <higor.a.gomes93@gmail.com>
2021-10-26 16:54:42 -0300

    Atualização

--- Atividade_Avaliativa_2.R
+++ Atividade_Avaliativa_2.R
@@ -28,7 +28,7 @@ plan
 X <- model.matrix(~C*T, data = plan[,-3])
 
 # Efeitos
-efeitos <- crossprod(X, y)/(2*2^2/2)
+efeitos <- crossprod(X, y)/(3*2^2/2)
 
 # Coeficientes
 coef <- efeitos/2
@@ -76,10 +76,64 @@ t_critico
 pvalue <- 2*pt(abs(t0), df = DFE, lower.tail = F)
 pvalue
 
+# Dataframe resumo do teste t
+testet <- data.frame(coef, rep(sqrt(MSE/N), 4), t0, pvalue)
+colnames(testet) <- c("Coef", "SE_coef", "t0", "p-value")
 
+# Coef de determinação múltipla
+R2 <- 1 - SSE/SST
+R2_aj <- 1 - MSE/MST
 
+#--------------------------------------------------------------------------------#
 
+## Anova
+# soma dos quadrados dos efeitos
+SS_x <- crossprod(X[,-1], y)^2/N
 
+# Média dos quadrados dos efeitos
+MS_x <- SS_x/1
 
+# F Calculado
+F0 <- MS_x/MSE
+F0
 
+# pvalor
+p <- pf(F0, 1, DFE, lower.tail = F)
+p
 
+# Tabela resumo ANOVA
+Source <- c("C", "T", "CT", "Erro", "Total")
+SS <- c(SS_x, SSE, SST)
+DF <- c(rep(1, 3), DFE, DFT)
+MS <- c(MS_x, MSE, MST)
+F0 <- c(F0, NA, NA)
+pvalor <- c(p, NA, NA)
+
+ANOVA <- data.frame(SS, DF, MS, F0, pvalor)
+rownames(ANOVA) <- Source
+ANOVA
+
+#--------------------------------------------------------------------------------#
+
+## Mínimos quadrados matricial
+# Multiplicando X transposta por X
+t(X)%*%X
+
+# Inversa do resultado anterior
+solve(t(X)%*%X)
+
+# Multiplicando X transposta por y
+t(X)%*%y
+
+# Obtendo os coeficientes
+beta_mat <- solve(t(X)%*%X)%*%t(X)%*%y
+beta_mat
+  
+# Previsão para todos os resultados experimentais
+y_hay <- X%*%beta_mat
+
+# Previsão para valores de C e T específicos
+# C = 0, T = 0.5
+x_esp <- matrix(c(1, 0, 0.5, 0), nrow = 4, ncol = 1)
+y_esp_hat <- t(x_esp)%*%beta_mat
+y_esp_hat
