50eb5f8fbbbed21c9c13eebeb04573a4f3616c58
Higor Gomes <higor.a.gomes93@gmail.com>
2021-10-26 15:28:27 -0300

    Atualização

---
+++ Atividade_Avaliativa_2.R
@@ -0,0 +1,25 @@
+# Planejamento Fatorial 2^2
+
+## Construção do Experimento
+# Níveis
+levels <- c(-1, 1)
+
+# Planejamento
+plan <- expand.grid(levels, levels)
+plan <- rbind(plan, plan, plan)
+
+# Nomes das colunas
+colnames(plan) <- c("C", "T")
+
+# Respostas
+y <- c(26.6, 40.9, 11.8, 34.0,
+       22.0, 36.4, 15.9, 29.0,
+       22.8, 36.7, 14.3, 33.6)
+
+plan$y <- y
+plan
+
+
+## Análise do Experimento
+# Matriz do Planejamento
+
